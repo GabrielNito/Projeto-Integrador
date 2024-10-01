8187809621fde60aef6a1730df9a9db17eb920cf
Caique Moreira <caique_moreira_sz@hotmail.com>
2024-10-01 03:06:26 -0300

    feat: mergin all routes

--- backend/Server.ts
+++ backend/Server.ts
@@ -9,10 +9,13 @@ const PORT = process.env.SERVER_PORT || 3001;
 const app = express();
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 =======
 >>>>>>> 9c0efe359207b1107eed05d7fa60e5f862feae7d
 app.use(express.json());
+=======
+>>>>>>> aae7d9c (feat: read route for threads | getAllThreads and getThreadsById)
 app.use('/api', routes);
 
 app.listen(PORT, () => console.log(`Server listening on http://localhost:${PORT}`));
--- backend/src/Routes/index.ts
+++ backend/src/Routes/index.ts
@@ -1,30 +1,17 @@
 import { Router } from 'express';
-import threadsRouter from './threads.route';
-<<<<<<< HEAD
-import visitsRouter from './visits.route';
-=======
-<<<<<<< HEAD
-import notificationsRouter from './notifications.route';
-=======
-<<<<<<< HEAD
-import usersRouter from './users.route';
-=======
-import postsRouter from './posts.route';
->>>>>>> ffa77fe8b78e6005d8ce8c420f411962c2ed3f52
 
->>>>>>> 9c0efe359207b1107eed05d7fa60e5f862feae7d
->>>>>>> f1da1dd20a1446f9a4ce15682d40f543f2474cfc
+import threadsRouter from './threads.route';
+import visitsRouter from './visits.route';
+import notificationsRouter from './notifications.route';
+import usersRouter from './users.route';
+import postsRouter from './posts.route';
+
 const router = Router();
 
 router.use('/threads', threadsRouter);
 router.use('/posts', postsRouter);
-
-<<<<<<< HEAD
 router.use('/visits', visitsRouter);
-
-=======
 router.use('/users', usersRouter);
-
 router.use('/notifications', notificationsRouter);
->>>>>>> f1da1dd20a1446f9a4ce15682d40f543f2474cfc
+
 export default router;
