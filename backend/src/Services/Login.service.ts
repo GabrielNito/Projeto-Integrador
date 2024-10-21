d952ab42ac841a89f862e0681cd69cd60b03657b
Caique Moreira <caique_moreira_sz@hotmail.com>
2024-10-21 19:08:40 -0300

    feat: login route | user authentication

---
+++ backend/src/Services/Login.service.ts
@@ -0,0 +1,35 @@
+import * as jose from 'jose';
+import bcrypt from 'bcrypt';
+import { UsersService } from './Users.service';
+
+export class LoginService {
+  private _userService = new UsersService();
+
+  async authenticate(email: string, password: string) {
+    const user = await this._userService.getUserByEmail(email);
+
+    if (!user) {
+      throw Error('Usuário não encontrado');
+    }
+
+    const isValidPassword = await bcrypt.compare(password, user.password);
+
+    if (!isValidPassword) {
+      throw Error('Invalid passoword');
+    }
+
+    const payload = { id: user.id, email: user.email };
+    const secret = new TextEncoder().encode(process.env.JWT);
+    const alg = 'HS1024';
+
+    const token = await new jose.SignJWT(payload)
+      .setProtectedHeader({ alg })
+      .setExpirationTime('30d')
+      .setIssuedAt()
+      .setIssuer('localhost://3001')
+      .setSubject('user')
+      .sign(secret);
+
+    return token;
+  }
+}
--- backend/src/middlewares/auth.middleware.ts
+++ backend/src/middlewares/auth.middleware.ts
@@ -13,19 +13,19 @@ declare module 'express-serve-static-core' {
 export const auth = async (req: Request, res: Response, next: NextFunction) => {
   const token = req.headers.authorization?.split(' ')[1];
   if (!token) {
-    return res.status(401).json({ message: 'token not found' });
+    res.status(401).json({ message: 'token not found' });
   }
 
   try {
     const secret = new TextEncoder().encode(process.env.JWT_SECRET);
-    const data = await jose.jwtVerify(token, secret);
+    const data = await jose.jwtVerify(token as string, secret);
     if (!data) {
-      return res.status(401).json({ message: 'invalid token' });
+      res.status(401).json({ message: 'invalid token' });
     }
 
     req.user = data.payload;
     next();
   } catch (error) {
-    return res.status(401).json({ message: 'invalid token' });
+    res.status(401).json({ message: 'invalid token' });
   }
 };
